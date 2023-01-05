9229fc6ce62fa5f9acf77d778f3820f651405e6c
Andreas Leurs <al@integer-net.de>
2023-01-05 09:12:15 +0100

    #26061 Started coding on Windows

--- src/Service/ScopeHintService.php
+++ src/Service/ScopeHintService.php
@@ -7,11 +7,12 @@ namespace IntegerNet\CliScopeHint\Service;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Store\Model\StoreManagerInterface;
+use IntegerNet\CliScopeHint\Service\TreePaths;
 
 class ScopeHintService
 {
-    private ScopeConfigInterface $scopeConfig;
-    private StoreManagerInterface $storeManager;
+    private ScopeConfigInterface                       $scopeConfig;
+    private StoreManagerInterface                      $storeManager;
 
     public function __construct(
         ScopeConfigInterface $scopeConfig,
@@ -21,10 +22,52 @@ class ScopeHintService
         $this->storeManager = $storeManager;
     }
 
+    public function getAllScopes() : array
+    {
+        //$configArray = $this->treePaths->generate($this->scopeConfig->getValue(null));
+
+        //$all_configs = $this->scopeConfig->getValue(null);
+
+
+        return [];
+    }
+
+    public function getScopesAsArray(): array
+    {
+        $result_array = [];
+
+        $scope_array = $this->scopeConfig->getValue(null);
+        foreach ($scope_array as $element)
+        {
+            $result_array[] = $this->getConfigPathName($element);
+        }
+
+        return $result_array;
+    }
+
+    private function getConfigPathName($element, string $previous_path = ""): string
+    {
+        if(is_array($element)) {
+            foreach ($element as $array_name=>$array_elements)
+            {
+                $previous_path .= '/' . $array_name;
+                $this->getConfigPathName($array_elements, $previous_path);
+            }
+        }
+        else
+        {
+            return $previous_path;
+        }
+
+        return "";
+    }
+
     public function getConfigValuesForScopes(
         string $configPath
     ): array {
         $configValues = [];
+
+        //$allConfigPaths = $this->getScopesAsArray();
 
         // global scope
         $configValues[] = [
